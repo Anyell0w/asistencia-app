c44db2c73aaff305ef9f08f0bd6b57efdb5b6f86
unknown <qw5nzwx@gmail.com>
2024-07-19 07:59:39 -0500

    version de prueba(con pdf_generator.py)

--- pdf_generator.py
+++ pdf_generator.py
@@ -1,21 +1,32 @@
-from reportlab.lib.pagesizes import letter
+from reportlab import pdfgen
 from reportlab.pdfgen import canvas
-import datetime
+from reportlab.lib.pagesizes import letter
+import datetime as date
 
-def generar_pdf(data, filename):
-    c = canvas.Canvas(filename, pagesize=letter)
-    width, height = letter
-    c.setFont("Helvetica", 12)
-    text = c.beginText(50, height - 50)
-    text.setFont("Helvetica", 12)
+class PDFGenerator:
+    def generate(filename, data):
+        width, height = letter
+        c = canvas.Canvas(filename, pagesize=letter)
+        text = c.beginText(50, height - 50)
+        text.setFont("Helvetica", 12)
+        
+        text.textLine("Reporte de Asistencia")
+        text.textLine(f"Generado el: {date.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+        text.textLine("")
+        
+        for row in data:
+            text.textLine(f"DNI: {row[1]}, Nombres: {row[2]}, Apellidos: {row[3]} {row[4]}, Fecha de Registro: {row[5]}")
 
-    text.textLine("Reporte de Asistencia")
-    text.textLine(f"Generado el: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    text.textLine("")
-
-    for row in data:
-        text.textLine(f"DNI: {row[1]}, Nombres: {row[2]}, Apellidos: {row[3]} {row[4]}, Fecha de Registro: {row[5]}")
-
-    c.drawText(text)
-    c.showPage()
-    c.save()
+        c.drawText(text)
+        c.save()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
